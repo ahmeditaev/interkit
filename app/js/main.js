c556a49182d34cade0f4bf1b818d4082aceeb708
Akhmed Itaev <ahmeditaev@gmail.com>
2020-12-03 05:41:44 +0600

    added scroll to top button

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,25 @@
 const $navLinks = document.querySelectorAll('.nav-menu-list__item a')
 const $nameFormField = document.getElementById('name-field')
+const $scrollToTopButton = document.getElementById('scroll-to-top')
+const heightOfPromoSection = document.getElementById('promo').offsetHeight
 
+//Appearance or disappearance of the scroll top button
+document.addEventListener('scroll', () => {
+  window.scrollY >= heightOfPromoSection ?
+    $scrollToTopButton.style.opacity = '1' :
+    $scrollToTopButton.style.opacity = '0'
+})
+
+//Scroll to top event
+$scrollToTopButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  scroll({
+    top: 0,
+    behavior: "smooth"
+  });
+})
+
+//Click handler for each nav links
 for (const link of $navLinks) {
   link.addEventListener("click", clickHandler);
 }
