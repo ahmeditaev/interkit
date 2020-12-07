dfb9d40be0a24348d8fae8eda9247c841a9134b1
Akhmed Itaev <ahmeditaev@gmail.com>
2020-12-08 00:27:02 +0600

    fixed height of mobile nav menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,44 +1,78 @@
-const $navLinks = document.querySelectorAll('.nav-menu-list__item a')
-const $nameFormField = document.getElementById('name-field')
-const heightOfPromoSection = document.getElementById('promo').offsetHeight
-const $header = document.getElementById('header')
-const $mainLogo = document.getElementById('main-logo')
-const $copyright = document.getElementById('copyright')
-
-const heightOfStickyHeader = 65;
-
-$copyright.innerHTML = `© INTERKIT - ${new Date().getFullYear()}`
-
-$mainLogo.addEventListener('click', clickHandler)
-
-document.addEventListener('scroll', () => {
-  return window.scrollY >= heightOfPromoSection - heightOfStickyHeader ?
-    $header.classList.add("header-fixed") :
-    $header.classList.remove("header-fixed")
-})
-
-//Click handler for each nav links
-for (const link of $navLinks) {
-  link.addEventListener("click", clickHandler);
+const setHeightToMobileNavMenu = () => {
+  const $navMenu = document.getElementById('nav-menu');
+  if (window.innerWidth <= 1024) {
+    const height = window.innerHeight //Instead of 100vh value. Mobile viewport bug fix
+    $navMenu.style.height = `${height}px`
+  } else {
+    $navMenu.style.height = 'unset'
+  }
 }
 
-function clickHandler(e) {
-  e.preventDefault();
-  const isFormSection = this.getAttribute("href") === '#form'
-  const href = this.getAttribute("href");
-  let offsetTop = document.querySelector(href).offsetTop;
+window.addEventListener('load', () => {
+  //Constants
+  const $navLinks = document.querySelectorAll('.nav-menu-list__item a'),
+        $nameFormField = document.getElementById('name-field'),
+        $header = document.getElementById('header'),
+        $mainLogo = document.getElementById('main-logo'),
+        $copyright = document.getElementById('copyright');
 
-  if (this.getAttribute('href') !== "#about") {
-    offsetTop = offsetTop - heightOfStickyHeader
+  const heightOfPromoSection = document.getElementById('promo').offsetHeight,
+        heightOfStickyHeader = 65;
+  //End constants
+
+  //Event listeners
+  document.addEventListener('scroll', stickyHeaderHandler)
+
+  $mainLogo.addEventListener('click', clickHandler)
+
+  for (const link of $navLinks) {
+    link.addEventListener("click", clickHandler);
+  }
+  //End event listeners
+  //--------------------------------------------------------------------------------------
+  $copyright.innerHTML = `© INTERKIT - ${new Date().getFullYear()}`
+  //--------------------------------------------------------------------------------------
+  //Functions
+  function clickHandler(e) {
+    e.preventDefault();
+    const isFormSection = this.getAttribute("href") === '#form'
+    const href = this.getAttribute("href");
+    let offsetTop = document.querySelector(href).offsetTop;
+
+    if (this.getAttribute('href') !== "#about") {
+      offsetTop = offsetTop - heightOfStickyHeader
+    }
+
+    scroll({
+      top: offsetTop,
+      behavior: "smooth"
+    });
+
+    setTimeout(() => {
+      isFormSection && $nameFormField.focus()
+    }, 1000)
   }
 
-  scroll({
-    top: offsetTop,
-    behavior: "smooth"
-  });
+  function stickyHeaderHandler() {
+    return window.scrollY >= heightOfPromoSection - heightOfStickyHeader ?
+      $header.classList.add("header-fixed") :
+      $header.classList.remove("header-fixed")
+  }
+  //End functions
 
-  setTimeout(() => {
-    isFormSection && $nameFormField.focus()
-  }, 1000)
-}
+  //Init
+  setHeightToMobileNavMenu()
+
+  // SVGInject(document.querySelectorAll("img.injectable"), {
+  //   beforeInject: function (img, svg) {
+  //     console.log(img.offsetWidth, img.offsetHeight, img.src )
+  //     svg.setAttribute('width', img.offsetWidth)
+  //     svg.setAttribute('height', img.offsetHeight)
+  //     svg.setAttribute('viewBox', `0 0 ${img.offsetWidth * 1.8} ${img.offsetHeight * 1.8}`)
+  //   }
+  // });
+  //End init
+})
+
+window.addEventListener('resize', setHeightToMobileNavMenu)
 
